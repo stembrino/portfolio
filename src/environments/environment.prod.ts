b6fac3f93a5f1246366211dcd947becce1e39c5b
stembrino <stembrino@gmail.com>
2020-01-27 14:14:02 +0000

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDx07-6dU9iuPwTz_o181jMWOkDzaOaEwY",
+    authDomain: "porfolio-fabio.firebaseapp.com",
+    databaseURL: "https://porfolio-fabio.firebaseio.com",
+    projectId: "porfolio-fabio",
+    storageBucket: "porfolio-fabio.appspot.com",
+    messagingSenderId: "1054670305623",
+    appId: "1:1054670305623:web:3bac030ddd6d79fc87a612",
+    measurementId: "G-W8V5537SL7"
+  }
 };
